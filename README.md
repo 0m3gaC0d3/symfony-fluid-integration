249552d1aa4ed3a934a97099360fcbcb240078cb
Wolf Utz <utz@riconet.de>
2018-08-14 22:56:58 +0200

    Remove lots of files
    Remove almost all files to simplify the project in the first versions.

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 # Symfony 4 - Fluid integration
 
+## ! This is currently work in progress. !
+
 ## Description
 Integrates the template engine fluid into symfony 4 projects.
 
-# ! Not working yet !
+## Setup
+Open the file ``config/services.yaml`` and add the following:
+````yaml
+services:
+  templating.engine.fluid:
+    public: true
+    autowire: false
+    class: OmegaCode\FluidIntegration\FluidEngine
+    arguments:
+    - '@kernel'
+````
+Next, open the file ``config/packages/framework.yaml`` and add the following:
+````yaml
+framework:
+  templating:
+    engines: ['fluid', 'twig', 'php']
+````
+Finally open the file ``config/bundles.php`` and add the bundle:
+````php
+<?php
+return [
+    ...
+    OmegaCode\FluidIntegration\OmegaCodeFluidIntegrationBundle::class => ['all' => true],
+    ...
+];
+```` 
--- src/FluidEngine.php
+++ src/FluidEngine.php
@@ -1,19 +1,40 @@
 <?php
+/**
+ * Copyright 2018 OmegaCode.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 namespace OmegaCode\FluidIntegration;
 
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Templating\TemplateNameParser;
 use Symfony\Component\Templating\TemplateNameParserInterface;
 use Symfony\Component\Templating\TemplateReferenceInterface;
-use TYPO3Fluid\Fluid\Core\ViewHelper\ViewHelperResolver;
 use TYPO3Fluid\Fluid\View\TemplateView;
 
 /**
  * Class FluidEngine.
+ *
+ * @TODO: Refactor this class and move methods: load, setRootPaths
  */
 class FluidEngine implements EngineInterface
 {
@@ -21,11 +42,6 @@ class FluidEngine implements EngineInterface
      * @var TemplateView
      */
     private $fluid;
-
-    /**
-     * @var ViewHelperResolver
-     */
-    private $viewHelperResolver;
 
     /**
      * @var TemplateNameParserInterface
@@ -45,16 +61,14 @@ class FluidEngine implements EngineInterface
     /**
      * FluidEngine constructor.
      *
-     * @param TemplateView                $fluid
-     * @param TemplateNameParserInterface $nameParser
-     * @param ContainerInterface          $container
+     * @param \App\Kernel $kernel
      */
-    public function __construct(TemplateView $fluid, TemplateNameParserInterface $nameParser, ContainerInterface $container)
+    public function __construct($kernel)
     {
-        $this->fluid = $fluid;
-        $this->nameParser = $nameParser;
-        $this->container = $container;
-        $this->kernel = $this->container->get('kernel');
+        $this->kernel = $kernel;
+        $this->container = $kernel->getContainer();
+        $this->fluid = $this->setRootPaths(new TemplateView());
+        $this->nameParser = new TemplateNameParser();
     }
 
     /**
@@ -68,7 +82,7 @@ class FluidEngine implements EngineInterface
      *
      * @throws \Exception
      */
-    public function renderResponse($view, array $parameters = array(), Response $response = null)
+    public function renderResponse($view, array $parameters = [], Response $response = null)
     {
         if (null === $response) {
             $response = new Response();
@@ -90,7 +104,7 @@ class FluidEngine implements EngineInterface
      * @throws \RuntimeException if the template cannot be rendered
      * @throws \Exception
      */
-    public function render($name, array $parameters = array())
+    public function render($name, array $parameters = [])
     {
         $this->fluid->assignMultiple($parameters);
         $this->fluid->getTemplatePaths()->setTemplatePathAndFilename($this->load($name));
@@ -136,17 +150,30 @@ class FluidEngine implements EngineInterface
      */
     protected function load($name)
     {
-        if (!preg_match('/^([^:]*):([^:]*):(.+)\.([^\.]+)\.*([^\.]*)$/', $name, $matches)) {
-            return $this->fluid->getTemplatePaths()->resolveTemplateFile($name);
+        $nameParts = explode('/', $name);
+        if (2 != count($nameParts)) {
+            throw new \InvalidArgumentException(
+                "The given template name *$name*  is not inf format CONTROLLER/ACTION.html",
+                1534278686
+            );
         }
+        $controller = $nameParts[0];
+        $action = $nameParts[1];
 
-        $bundle = $this->kernel->getBundle($matches[1]);
-        if (!$bundle instanceof Bundle) {
-            throw new \Exception('Could not find a Bundle named "'.$matches[1].'"');
-        }
+        return $this->fluid->getTemplatePaths()->resolveTemplateFileForControllerAndActionAndFormat($controller, $action);
+    }
 
-        $this->fluid->getTemplatePaths()->addBasePath($bundle->getPath());
+    /**
+     * @param TemplateView $templateView
+     *
+     * @return TemplateView
+     */
+    private function setRootPaths(TemplateView $templateView)
+    {
+        $templateView->getTemplatePaths()->setTemplateRootPaths([$this->kernel->getRootDir().'/../res/private/templates']);
+        $templateView->getTemplatePaths()->setLayoutRootPaths([$this->kernel->getRootDir().'/../res/private/layouts']);
+        $templateView->getTemplatePaths()->setPartialRootPaths([$this->kernel->getRootDir().'/../res/private/partials']);
 
-        return $this->fluid->getTemplatePaths()->resolveTemplateFileForControllerAndActionAndFormat($matches[2], $matches[3]);
+        return $templateView;
     }
 }
